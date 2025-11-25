abe73d7477f713fafb6ffa65f04419713e86081a
WPF Pesho <petyr.lubomirov.petrov@gmail.com>
2025-11-25 20:17:24 +0200

    Add project dependencies

--- src/Server.WebApi/Program.cs
+++ src/Server.WebApi/Program.cs
@@ -1,12 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace Server.WebApi;
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        // Authentication/Authorization will be configured later
+        builder.Services.AddAuthentication();
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
