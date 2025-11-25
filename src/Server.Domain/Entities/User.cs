ee338e38360fd248b42a2bfad4c1f64aaeacffe6
WPF Pesho <petyr.lubomirov.petrov@gmail.com>
2025-11-25 21:21:58 +0200

    Created User and UserRole entities

---
+++ src/Server.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+namespace Server.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string Email { get; set; } = default!;
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+
+    public DateTime CreatedAt { get; set; }
+    public bool IsActive { get; set; }
+}
